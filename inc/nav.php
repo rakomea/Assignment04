986c738b5a67224071716c859eade9551483b5a2
rakomea <55210364+rakomea@users.noreply.github.com>
2019-10-09 00:52:41 -0400

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,8 @@
+<nav class=" n">
+	<ul class="menu">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="dewey-bazirake.php">Dewey Bazirake</a></li>
+		<li><a href="jialin-huo.php">Jailin Huo</a></li>
+		<li><a href="joshua-wolkoff.php">Joshua Wolkoff</a></li>
+	</ul>
+</nav>
