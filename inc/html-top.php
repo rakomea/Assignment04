2513178c2454fe5f819c0bbd086a3dfe1490ad4f
vacevedo44 <55210725+vacevedo44@users.noreply.github.com>
2019-10-09 11:13:38 -0400

    Add files via upload

--- inc/html-top.php
+++ inc/html-top.php
@@ -6,9 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $pageTitle;?></title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<link rel="stylesheet" type="text/css" href="css/stylestwo.css">
 	<link rel="shortcut icon" href="../favicon.ico">
-	<link rel="stylesheet" type="text/css" href="css/default.css" />
 	<link rel="stylesheet" type="text/css" href="css/component.css" />
 	<script src="js/modernizr.custom.js"></script>
 
