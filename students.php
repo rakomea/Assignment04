c0b07be5c0bd1f878136cce024dd41a15915ed2f
vacevedo44 <55210725+vacevedo44@users.noreply.github.com>
2019-10-09 17:38:01 -0400

    Add files via upload
    fixed classes

--- students.php
+++ students.php
@@ -7,38 +7,41 @@
 	<h1 class="box h sub">Meet the Students</h1>
 </header>
 
-<nav class=" box n "><a href="index.php">Back to Brooklyn!</a></nav>
+<nav class=" box menu "><a href="index.php">Back to Brooklyn!</a></nav>
 
 <main>
-	<section class="box f">
+	<section class="box">
 	<img src="images/dewey-bazirake.jpg" alt="Dewey Bazirake">
 
-		<article>
+		<article class="f">
+			<div class="headings">
 			<h2 class="name">Dewey Bazirake</h2>
 			<h3 class="compsci">Computer Science major</h3>
-
+		</div>
 			<p>My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. They focused on image processing of athletes in marathons. It is through this experience I got a chance to learn more about Deep Learning, Artificial Intelligence & the power of Big Data. From then on, my life has never been the same again. I am always looking for opportunities to enhance my knowledge and skills in the Data & the Technology sector. The language I am getting fluent at: python, statistics, machine learning and databases. My hope is that I can work work with numerous companies who have access to large data so that I can do what I do best ~ organize it, analyze it & interpret it to derive the best value.</p>
 		</article>
 	</section>
 
-	<section class="f odd">
+	<section class="odd">
 		<img src="images/jialin-huo.jpg" alt="Jialin Huo">
 
-		<article>
+		<article class="f">
+			<div class="oddhead">
 			<h2 class="name">Jialin Huo</h2>
 			<h3 class="compsci">Computer Science minor</h3>
-
+		</div>
 			<p>As a fifth-year student at the University of Rochester, I am double majoring in Business Finance and Art History, with a minor in Computer Science. Due to computer science and coding’s popularity and its extensive use in all kinds of fields and the business world, I have decided to re-classify from Class of 2019 to Class of 2020 in order to complete this challenging but extremely useful minor and coding abilities. Until now, I have acquired coding experience including Python, R Studio, HTML, CSS and little knowledge of JavaScript Plugins and I am thrilled to learn more from both my professors and classmates.</p>
 		</article>
 	</section>
 
-	<section class="box f">
+	<section class="box ">
 		<img src="images/joshua-wolkoff.jpg" alt="Joshua Wolkoff">
 
-		<article>
+		<article class="f">
+			<div class="headings">
 			<h2 class="name">Joshua Wolkoff</h2>
 			<h3 class="compsci">Computer Science major</h3>
-
+		</div>
 			<p>I am a senior majoring in computer science and philosophy. Ever since my sophomore year when I took Philosophy of Mind (I highly recommend this class) I have been extremely interested in consciousness and concepts like free will and determinism. I pursued this idea more in computer science classes like Artificial Intelligence and Machines and Consciousness. I have taken many other computer science classes throughout the last three years including: 170 (obviously), 172, 173, 214, 252 and 278.</p>
 		</article>
 	</section>
